48798bcacee71c258cceac268d6cadb1d2227da3
Meriem Ben Zouitine <benzouitinemeriem@gmail.com>
2026-05-07 10:30:11 +0100

    Updated gallery, login and database setup

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -21,7 +21,7 @@ function dbConnect() {
 }
 $pages = array(
     '/' => array('file' => 'home', 'text' => 'Mainpage', 'menun' => array(1,1)),
-    'images' => array('file' => 'images', 'text' => 'Images', 'menun' => array(1,1)),
+    'gallery' => array('file' => 'images', 'text' => 'Images', 'menun' => array(1,1)),
     'contact' => array('file' => 'contact', 'text' => 'Contact', 'menun' => array(1,1)),
     'messages' => array('file' => 'messages', 'text' => 'Messages', 'menun' => array(0,1)),
     'crud' => array('file' => 'crud', 'text' => 'CRUD', 'menun' => array(1,1)),
